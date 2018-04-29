6333a2248961357365c442ae9083a4d438727396
Joris Kok <joris_kok1@hotmail.com>
2018-04-29 15:24:04 +0200

    pypi preperations

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# chinese2wordlist
+# Chinese2wordlist
 Python package to convert chinese text to a word list
+
+### Example
+> `python chinese2wordlist.py trad 我是荷蘭人`
+
+Which returns a json string with all found translations in CC-CEDICT 
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='chinese2wordlist',
+    packages=['chinese2wordlist'],
+    version='0.1',
+    description='Input chinese text to return a word list to use with learning',
+    author='Joris Kok',
+    author_email='joris.kok1@hotmail.com',
+    url='https://github.com/JorisKok/chinese2wordlist',
+    download_url='https://github.com/JorisKok/chinese2wordlist/archive/0.1/0.1.tar.gz',
+    keywords=['chinese', 'learning'],
+    classifiers=[],
+)
